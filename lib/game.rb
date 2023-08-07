334c2e369c5046325f6573d581c8f003dab17c02
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-07 20:58:54 -0300

    Add can_be_archived method thuat returns true AND if last_played_at is older than 2 years

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative 'item'
+require 'time'
 
 # this file holds the methods of the game items
 class Game < Item
@@ -7,10 +8,10 @@ class Game < Item
   def initialize(multiplayer, last_played_at)
     super(published_date: Time.now)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Time.parse(last_played_at)
   end
 
   def can_be_archived?
-    archived?
+    super && (Time.now - @last_played_at) > 2 * 365 * 24 * 60 * 60
   end
 end
