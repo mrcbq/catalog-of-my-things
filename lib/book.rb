f5543c95676e15085676080d4affe84d9aaa0db7
Romans S <romans.spilaks@gmail.com>
2023-08-08 02:28:12 +0300

    Add method to list all books

--- lib/book.rb
+++ lib/book.rb
@@ -9,6 +9,23 @@ class Book < Item
     @cover_state = options[:cover_state]
   end
 
+  def self.list_all_books(books)
+    puts "All Books:"
+    books.each do |book|
+      if books.empty?
+        puts 'No books added yet'
+      else
+        books.each do |book|
+          puts "Book ID: #{book.id}"
+          puts "Book Name: #{book.label.title}"
+          puts "Author: #{book.author.first_name} #{book.author.last_name}"
+          puts "Published by: #{book.publisher} on: #{book.publish_date}"
+          puts "Genre: #{book.genre.name}"
+          puts "Color: #{book.label.color}/n"
+        end
+      end
+  end
+
   private
 
   def can_be_archived?
