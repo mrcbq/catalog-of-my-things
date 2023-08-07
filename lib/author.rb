677b3f6e79b1ccc33e846fa75b233a7e9372f8c2
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-07 20:05:14 -0300

    Author class properties visible in the diagram defined in the constructor method.

--- lib/author.rb
+++ lib/author.rb
@@ -2,4 +2,17 @@ require_relative 'item'
 
 # associated to the item class
 class Author < Item
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    super(published_date: Time.now)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
 end
+
