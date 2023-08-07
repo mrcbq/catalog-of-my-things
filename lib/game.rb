c84bd10a8fb4363a53d4e300daa3a6c61a711045
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-07 19:52:49 -0300

    Game class properties visible in the diagram defined in the constructor method.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,16 @@
+require_relative 'item'
+
 # this file holds the methods of the game items
-class Game
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at)
+    super(published_date: Time.now)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    archived?
+  end
 end
