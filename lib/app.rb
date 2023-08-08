f45ab0b7f8653aa21963b032707ad00f0c45c6c3
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-08 15:17:56 -0300

    Fix linter errors.

--- lib/app.rb
+++ lib/app.rb
@@ -21,46 +21,45 @@ class App
     @authors = load_authors
   end
 
-     def list_all_genres()
-      @genres.each_with_index do |genre, index|
-        puts "#{index + 1}. #{genre.name}"
-      end
+  def list_all_genres
+    @genres.each_with_index do |genre, index|
+      puts "#{index + 1}. #{genre.name}"
+    end
+  end
+
+  def list_all_labels
+    Label.list_all_labels(@labels)
+  end
+
+  def list_all_music_albums
+    puts 'List of all music albums:'
+    @music_albums.each_with_index do |album, index|
+      next unless album.is_a?(MusicAlbum)
+
+      puts "#{index + 1}. #(Published: #{album.published_date}, Archived: #{album.archived?})"
+    end
+  end
+
+  def add_music_album
+    print 'Enter published date YYYY-MM-DD: '
+    date_input = gets.chomp
+    begin
+      published_date = Date.parse(date_input)
+      current_date = Date.today
+      difference = (current_date - published_date).to_i / 365
+
+      puts "The album was published #{difference} years ago"
+    rescue ArgumentError
+      puts 'invalid date format. Please enter the date in YYYY-MM-DD format'
     end
 
-    def list_all_labels
-      Label.list_all_labels(@labels)
-    end
+    print 'Is it on Spotify? (true/false): '
+    on_spotify = gets.chomp.downcase == 'true'
 
-    def list_all_music_albums()
-      puts 'List of all music albums:'
-      @music_albums.each_with_index do |album, index|
-        next unless album.is_a?(MusicAlbum)
+    music_album = MusicAlbum.new(published_date: published_date, on_spotify: on_spotify)
+    music_albums << music_album
 
-        puts "#{index + 1}. #(Published: #{album.published_date}, Archived: #{album.archived?})"
-      end
-    end
-
-    def add_music_album()
-      print 'Enter published date YYYY-MM-DD: '
-      date_input = gets.chomp
-      begin
-        published_date = Date.parse(date_input)
-        current_date = Date.today
-        difference = (current_date - published_date).to_i / 365
-
-        puts "The album was published #{difference} years ago"
-      rescue ArgumentError
-        puts 'invalid date format. Please enter the date in YYYY-MM-DD format'
-      end
-
-      print 'Is it on Spotify? (true/false): '
-      on_spotify = gets.chomp.downcase == 'true'
-
-      music_album = MusicAlbum.new(published_date: published_date, on_spotify: on_spotify)
-      music_albums << music_album
-
-      puts 'Music album added successfully!'
-    end
+    puts 'Music album added successfully!'
   end
 
   def list_all_books
