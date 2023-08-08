914230844e1be598be68c688e33f3a22811efca3
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-07 22:22:14 -0300

    Add initialize method for game and method lo list games.

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,17 @@
 require_relative 'item'
+require_relative 'game'
 
 class App
+  def initialize
+    @games = []
+  end
+
+  def list_of_games
+    puts ''.center(50, '*')
+    puts 'The list is empty, please create a game!' if @games.empty?
+    @games.each { |ele| puts "Title: #{ele.multiplayer}, Author: #{ele.last_played_at}" }
+  end
+
   def exit_program
     puts 'Exiting the program...'
     exit
