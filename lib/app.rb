9fc9f6a023d5e0abf22386e0e11d247d13538d00
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-07 22:39:48 -0300

    Create method to add a game.

--- lib/app.rb
+++ lib/app.rb
@@ -11,13 +11,25 @@ class App
   def list_of_games
     puts ''.center(50, '*')
     puts 'The list is empty, please create a game!' if @games.empty?
-    @games.each { |ele| puts "Title: #{ele.multiplayer}, Author: #{ele.last_played_at}" }
+    @games.each { |ele| puts "Multiplayer: #{ele.multiplayer}, Last played at: #{ele.last_played_at}" }
   end
 
   def list_all_authors
     puts ''.center(50, '*')
     puts 'The list is empty, please create a author!' if @authors.empty?
     @authors.each { |ele| puts "First Name: #{ele.first_name}, Last Name: #{ele.last_name}" }
+  end
+
+  def add_game
+    puts 'Add a game'
+    print 'Multiplayer: '
+    player = gets.chomp.capitalize
+    print "Please enter a date Last played at 'YYYY-MM-DD': "
+    input = gets.chomp.strip
+    game = Game.new(player, input)
+    @games << game
+    puts 'Successfully created a game!'
+    puts
   end
 
   def exit_program
