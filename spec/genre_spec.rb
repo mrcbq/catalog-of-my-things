5d7abcffd985eb2d3ac52fbcf5216989b169d4d4
mrcbq <marcobaquero@gmail.com>
2023-08-08 16:47:47 -0500

    Add test for @Genre.initialize method

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,2 +1,12 @@
-require_relative 'genre'
-require_relative 'item'
+require_relative '../lib/genre'
+require_relative '../lib/item'
+
+RSpec.describe Genre do
+  describe '#initialize' do
+    it 'creates a genre with the given name' do
+      genre = Genre.new('Science Fiction')
+      expect(genre.name).to eq('Science Fiction')
+    end
+  end
+
+end
