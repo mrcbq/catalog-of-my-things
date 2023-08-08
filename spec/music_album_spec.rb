f94120f340d7635d00da6c0366a6fff6aec95946
mrcbq <marcobaquero@gmail.com>
2023-08-08 12:41:17 -0500

    Add second test for music_album

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -4,11 +4,17 @@ require 'date'
 RSpec.describe MusicAlbum do
   let(:published_date) { Date.new(2000, 1, 1) }
 
-  context 'when creating a new MusicAlbum' do
+  describe 'when creating a new MusicAlbum' do
     it 'has the correct published_date and on_spotify attributes' do
       album = MusicAlbum.new(published_date: published_date, on_spotify: true)
       expect(album.published_date).to eq(published_date)
       expect(album.on_spotify).to be true
+    end
+
+    it 'defaults on_spotify attribute to false' do
+      album = MusicAlbum.new(published_date: published_date)
+
+      expect(album.on_spotify).to be false
     end
   end
 end
