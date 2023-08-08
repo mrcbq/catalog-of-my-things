a689622cc223d8b6479bafadce58c5c304ab42e9
mrcbq <marcobaquero@gmail.com>
2023-08-08 12:31:58 -0500

    Add test for check correct date and on_spotify properties

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,4 +2,12 @@ require_relative '../lib/music_album'
 
 RSpec.describe MusicAlbum do
   let(:published_date) { Date.new(2000, 1, 1) }
+
+  describe 'when creating a new MusicAlbum' do
+    it 'has the correct published_date and on_spotify attributes' do
+      album = MusicAlbum.new(published_date: published_date, on_spotify: true)
+      
+      expect(album.published_date).to eq(published_date)
+      expect(album.on_spotify).to be true
+    end
 end
