8cd0ab8534ac661b2043c6c9c51597aa245a229e
Romans S <romans.spilaks@gmail.com>
2023-08-08 21:52:01 +0300

    Add author and genre props and input promts to the class Book. Pass new proprs on creation of instance

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ require_relative 'item'
 require_relative 'label'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state, :label
+  attr_accessor :publisher, :cover_state, :label, :genre, :author
 
   def initialize(options = {})
     super(published_date: options[:published_date])
@@ -10,6 +10,8 @@ class Book < Item
     @id = Random.rand(1..1000)
     @cover_state = options[:cover_state]
     @label = options[:label]
+    @genre = options[:genre]
+    @author = options[:author]
   end
 
   def self.list_all_books(books)
@@ -21,7 +23,8 @@ class Book < Item
         books.each do |book|
           puts "Book ID: #{book.instance_variable_get(:@id)}"
           puts "Book Name: #{book.label.title}"
-          # puts "Author: #{book.author.first_name} #{book.author.last_name}"
+          puts "Author: #{book.author.first_name} #{book.author.last_name}"
+          puts "Genre: #{book.genre.name}" if book.genre
           puts "Published by: #{book.publisher} on: #{book.published_date}"
           puts "Color: #{book.label.color}\n"
         end
@@ -29,16 +32,34 @@ class Book < Item
     end
   end
 
-  def add_book(books, labels)
+  def add_book(books, labels, genres, authors)
     title = book_title
     publisher = input_publisher
     cover_state = input_cover_state
     published_date = input_published_date
     label = input_label(labels)
+    genre = input_genre(genres)
+    author = input_author(authors)
 
-    book = create_book(title, publisher, cover_state, published_date, label)
+    book = create_book(title, publisher, cover_state, published_date, label, genre, author)
     books << book
     puts 'Book added successfully!'
+  end
+
+  def save_books_to_json(filename)
+    File.open(filename, 'w') do |file|
+      file.puts JSON.dump(@books)
+    end
+    puts 'Books data saved to JSON.'
+  end
+
+  def load_books_from_json(filename)
+    if File.exist?(filename)
+      @books = JSON.parse(File.read(filename), symbolize_names: true)
+      puts 'Books data loaded from JSON.'
+    else
+      puts 'No JSON data found. Starting with an empty book list.'
+    end
   end
 
   private
@@ -56,6 +77,28 @@ class Book < Item
   def input_cover_state
     print 'Enter cover state (good/bad): '
     gets.chomp
+  end
+
+  def input_genre(genres)
+    print 'Select a genre:'
+    genres.each_with_index { |genre, index| puts "#{index + 1}. #{genre.name}" }
+    genre_index = gets.chomp.to_i - 1
+    genres[genre_index] if (0...genres.length).include?(genre_index)
+  end
+
+  def input_author(authors)
+    print 'Enter author first name:'
+    first_name = gets.chomp
+    print 'Enter author last name:'
+    last_name = gets.chomp
+
+    author = authors.find { |a| a.first_name == first_name && a.last_name == last_name }
+    unless author
+      author = Author.new(first_name, last_name)
+      authors << author
+    end
+
+    author
   end
 
   def input_published_date
@@ -78,13 +121,15 @@ class Book < Item
     label
   end
 
-  def create_book(title, publisher, cover_state, published_date, label)
+  def create_book(title, publisher, cover_state, published_date, label, genre, author)
     Book.new(
       title: title,
       publisher: publisher,
       cover_state: cover_state,
       published_date: published_date,
-      label: label
+      label: label,
+      genre: genre,
+      author: author
     )
   end
 
