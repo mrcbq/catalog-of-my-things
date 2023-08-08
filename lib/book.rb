1315f761d38282988af867ca31838edec0567bff
Romans S <romans.spilaks@gmail.com>
2023-08-08 14:03:22 +0300

    Add methods add_book and list_all_books

--- lib/book.rb
+++ lib/book.rb
@@ -10,20 +10,51 @@ class Book < Item
   end
 
   def self.list_all_books(books)
-    puts "All Books:"
-    books.each do |book|
+    puts 'All Books:'
+    books.each do |_book|
       if books.empty?
         puts 'No books added yet'
       else
         books.each do |book|
           puts "Book ID: #{book.id}"
-          puts "Book Name: #{book.label.title}"
-          puts "Author: #{book.author.first_name} #{book.author.last_name}"
+          # puts "Book Name: #{book.label.title}"
+          # puts "Author: #{book.author.first_name} #{book.author.last_name}"
           puts "Published by: #{book.publisher} on: #{book.publish_date}"
-          puts "Genre: #{book.genre.name}"
+          # puts "Genre: #{book.genre.name}"
           puts "Color: #{book.label.color}/n"
         end
       end
+    end
+  end
+
+  def self.add_book
+    print 'Enter book title: '
+    gets.chomp
+
+    print 'Enter publisher: '
+    publisher = gets.chomp
+
+    print 'Enter cover state (good/bad): '
+    cover_state = gets.chomp
+
+    print 'Enter published date YYYY-MM-DD: '
+    date_input = gets.chomp
+    begin
+      published_date = Date.parse(date_input)
+    rescue ArgumentError
+      puts 'Invalid date format. Please enter the date in YYYY-MM-DD format'
+      return
+    end
+
+    new_book = Book.new(
+      publisher: publisher,
+      cover_state: cover_state,
+      published_date: published_date
+    )
+
+    @books << new_book
+
+    puts 'Book added successfully!'
   end
 
   private
